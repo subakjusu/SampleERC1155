2c6483ff0e6fe23c0363da21f6f12648fef5502e
subakjusu <thirtytwoyams@gmail.com>
2022-01-20 16:17:06 +0000

    Modify mint method so only msg.sender can mint

--- contracts/Erc1155.sol
+++ contracts/Erc1155.sol
@@ -1,52 +1,33 @@
-// Contract based on https://docs.openzeppelin.com/contracts/4.x/erc1155
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Erc1155 is ERC1155 {
-    // GOLD_ID = 0000000000000000000000000000000000000000000000000000000000000000;
-    // SILVER_ID = 0000000000000000000000000000000000000000000000000000000000000001;
 
     constructor() ERC1155("https://raw.githubusercontent.com/subakjusu/SampleERC1155/master/token/{id}.json") {}
 
-    function mintCaller(uint256 tokenId, uint256 amount) public {
+    function mint(uint256 tokenId, uint256 amount) public {
         _mint(msg.sender, tokenId, amount, "");
     }
 
-    function mintOwner(uint256 tokenId, uint256 amount, address owner) public {
-        _mint(owner, tokenId, amount, "");
+    function mintWithData(uint256 tokenId, uint256 amount, bytes memory data) public {
+        _mint(msg.sender, tokenId, amount, data);
     }
 
-    function mintOwnerWithData(uint256 tokenId, uint256 amount, address owner, bytes memory data) public {
-        _mint(owner, tokenId, amount, data);
-    }
-
-    function mintSelfBatch(uint256[] memory tokenIds, uint256[] memory amounts) public {
+    function mintBatch(uint256[] memory tokenIds, uint256[] memory amounts) public {
         _mintBatch(msg.sender, tokenIds, amounts, "");
     }
 
-    function mintOwnerBatch(uint256[] memory tokenIds, uint256[] memory amounts, address owner) public {
-        _mintBatch(owner, tokenIds, amounts, "");
+    function mintWithDataBatch(uint256[] memory tokenIds, uint256[] memory amounts, bytes memory data) public {
+        _mintBatch(msg.sender, tokenIds, amounts, data);
     }
 
-    function mintOwnerWithDataBatch(uint256[] memory tokenIds, uint256[] memory amounts, address owner, bytes memory data) public {
-        _mintBatch(owner, tokenIds, amounts, data);
-    }
-
-    function burnCaller(uint256 tokenId, uint256 amount) public {
+    function burn(uint256 tokenId, uint256 amount) public {
         _burn(msg.sender, tokenId, amount);
     }
 
-    function burnOwner(uint256 tokenId, uint256 amount, address owner) public {
-        _burn(owner, tokenId, amount);
-    }
-
-    function burnCallerBatch(uint256[] memory tokenIds, uint256[] memory amounts) public {
+    function burnBatch(uint256[] memory tokenIds, uint256[] memory amounts) public {
         _burnBatch(msg.sender, tokenIds, amounts);
-    }
-
-    function burnOwnerBatch(uint256[] memory tokenIds, uint256[] memory amounts, address owner) public {
-        _burnBatch(owner, tokenIds, amounts);
     }
 }
